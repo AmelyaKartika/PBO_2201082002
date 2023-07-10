8b68a46310d19126de80bc303697cd52fc933000
Amelya Kartika <126840619+AmelyaKartika@users.noreply.github.com>
2023-07-10 21:32:48 +0700

    Modul 12
    Praktek

---
+++ PBO_2201082002/src/AmelTeori/Square.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AmelTeori;
+
+/**
+ *
+ * @author hp
+ */
+public class Square extends Shape {
+    
+    
+    public String getName(){
+        return "Persegi Panjang";
+    }
+   
+   
+    public double getArea(){
+      
+        return 0;
+    }
+    }
+
